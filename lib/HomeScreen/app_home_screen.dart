ccd8b7ae621423eed655bed179f3c8b699273365
sagor2315 <eeezahidul@gmail.com>
2026-03-11 15:05:19 +0600

    project setup and homescreen created

---
+++ lib/HomeScreen/app_home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ToDoAppHomeScreen extends StatefulWidget {
+  const ToDoAppHomeScreen({super.key});
+
+  @override
+  State<ToDoAppHomeScreen> createState() => _ToDoAppHomeScreenState();
+}
+
+class _ToDoAppHomeScreenState extends State<ToDoAppHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
